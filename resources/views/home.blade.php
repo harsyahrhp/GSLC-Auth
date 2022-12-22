3311ddee1447dadd9d1594a15d66d9d203ef039b
Harsya <harsyaharahap@gmail.com>
2022-12-22 09:24:52 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://akcdn.detik.net.id/community/media/visual/2021/05/13/pilihan-warna-baru-vespa-sprint-2021-1_43.png?w=250&q=" class="card-img-top" alt="biru">
+                    <div class="card-body">
+                        <h5 class="card-title">Vespa Sprint</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://imgcdnblog.carbay.com/wp-content/uploads/2020/07/28185349/2020-Vespa-Primavera-i-get-ABS-Green-Relax.jpg" class="card-img-top" alt="biru">
+                    <div class="card-body">
+                        <h5 class="card-title">Vespa Primavera</h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://asset.kompas.com/crops/QummqBVEM38FodeezdvTnH0GCjg=/0x45:1366x955/750x500/data/photo/2021/08/10/61120efee1753.png" class="card-img-top" alt="biru">
+                    <div class="card-body">
+                        <h5 class="card-title">Vespa GTS</h5>
+                    </div>
+                </div>
+            </div>
+
+    </div>
+
+</div>
+
+
+
+
+@endsection
